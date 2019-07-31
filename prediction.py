7ac70c9a9da878d7c41ddf420351499454b2f552
rbarbazz <raphael.barbazza@gmail.com>
2019-07-31 15:56:13 +0200

    working version

--- prediction.py
+++ prediction.py
@@ -1,20 +1,26 @@
 import sys
 import pandas as pd
 
-txt_input = input('Please input a car mileage: ')
 
-try:
-    mileage = float(txt_input)
-except ValueError:
-    sys.exit('Input is not a number')
+# Get last computed thetas if exist
+def get_thetas():
+    try:
+        thetas = pd.read_csv('thetas.csv')
+    except:
+        thetas = pd.DataFrame({'t0': [0], 't1': [0]})
+        thetas.to_csv('thetas.csv', mode='w', header=['t0', 't1'], index=False)
+    t0 = thetas.iloc[-1]['t0']
+    t1 = thetas.iloc[-1]['t1']
+    return t0, t1
 
-thetas = pd.read_csv('thetas.csv')
-t0 = thetas.iloc[-1]['t0']
-t1 = thetas.iloc[-1]['t1']
+if __name__ == '__main__':
+    txt_input = input('Please input a car mileage: ')
+    try:
+        mileage = float(txt_input)
+    except ValueError:
+        sys.exit('Input is not a number')
 
-price_prediction = t0 + (t1 * mileage)
-print('Thetas: {} and {}\nEstimated price for this mileage: {}'.format(
-    t0,
-    t1,
-    int(price_prediction)
-))
+    t0, t1 = get_thetas()
+
+    price_prediction = t0 + (t1 * mileage)
+    print('Estimated price for this mileage: {}'.format(int(price_prediction)))
--- training.py
+++ training.py
@@ -1,42 +1,55 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
+from prediction import get_thetas
 
-dataset = pd.read_csv('data.csv')
-X = dataset['km']
-Y = dataset['price']
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train the model')
+    parser.add_argument(
+        '-n',
+        help='Display data after training',
+        action='store_true',
+        default=False
+    )
+    args = parser.parse_args()
 
-thetas = pd.read_csv('thetas.csv')
-t0 = thetas.iloc[-1]['t0']
-t1 = thetas.iloc[-1]['t1']
+    # Read data from provided CSV file
+    dataset = pd.read_csv('data.csv')
+    X = dataset['km']
+    Y = dataset['price']
 
-iterations = 1000
-learning_rate = 0.1
-n = float(len(dataset['km']))
+    t0, t1 = get_thetas()
+    iterations = 1000
+    learning_rate = 0.1
+    n = float(len(dataset['km']))
 
-# Normalization
-X_norm = ((X - X.min()) / (X.max() - X.min()))
+    # Normalization
+    X_norm = ((X - X.min()) / (X.max() - X.min()))
 
-for i in range(iterations):
-    Y_pred = t0 + (t1 * X_norm)
-    tmp_t0 = (1/n) * sum(Y_pred - Y)
-    tmp_t1 = (1/n) * sum(X_norm * (Y_pred - Y))
-    t0 -= learning_rate * tmp_t0
-    t1 -= learning_rate * tmp_t1
+    for i in range(iterations):
+        Y_pred = t0 + (t1 * X_norm)
+        tmp_t0 = (1/n) * sum(Y_pred - Y)
+        tmp_t1 = (1/n) * sum(X_norm * (Y_pred - Y))
+        t0 -= learning_rate * tmp_t0
+        t1 -= learning_rate * tmp_t1
 
-# # Find the "unormalized" thetas
-# t0 += (t1 * X.min()) / (X.max() - X.min())
-# t1 = t1 / (X.max() - X.min())
+    # Find the "unormalized" thetas
+    t0_norm = t0
+    t1_norm = t1
+    t0 = t1_norm * - X.min() / (X.max() - X.min()) + t0_norm
+    t1 = t1_norm * (1 - X.min()) / (X.max() - X.min()) + t0_norm - t0
 
-# Save the thetas found
-thetas = pd.DataFrame({
-    't0': [t0],
-    't1': [t1]
-})
-thetas.to_csv('thetas.csv', mode='a', header=False, index=False)
+    # Save the thetas found
+    thetas = pd.DataFrame({
+        't0': [t0],
+        't1': [t1]
+    })
+    thetas.to_csv('thetas.csv', mode='a', header=False, index=False)
 
-# Plot data
-Y_pred = t0 + (t1 * ((X - X.min()) / (X.max() - X.min())))
-plt.figure(num='Linear Regression')
-plt.scatter(X, Y)
-plt.plot([max(X), min(X)], [min(Y_pred), max(Y_pred)], color='red')
-plt.show()
+    if (args.n):
+        # Plot data
+        Y_pred = t0 + (t1 * X)
+        plt.figure(num='Linear Regression')
+        plt.scatter(X, Y)
+        plt.plot([max(X), min(X)], [min(Y_pred), max(Y_pred)], color='red')
+        plt.show()
